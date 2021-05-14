d3267e698331fd9d823416dad2ffaf3a82805743
drumath <2237k.drumath@gmail.com>
2021-05-15 02:17:21 +0900

    :sparkles: webxr有効にしたりボタン追加したり

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { Engine, MeshBuilder, Scene } from "@babylonjs/core";
+import { ArcRotateCamera, Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene, Vector3 } from "@babylonjs/core";
+import { GUI3DManager, HolographicButton } from "@babylonjs/gui";
 import "./style/style.scss";
 
 const renderCanvas = <HTMLCanvasElement>document.getElementById("renderCanvas");
@@ -7,9 +8,41 @@ if (renderCanvas) {
   const engine = new Engine(renderCanvas, true, {});
   const scene = new Scene(engine);
 
-  MeshBuilder.CreateBox("box", {}, scene);
-  scene.createDefaultCameraOrLight(true, true, true);
-  scene.createDefaultEnvironment();
+  // MeshBuilder.CreateBox("box", {size:0.1}, scene);
+  // scene.createDefaultCameraOrLight(true, true, true);
+
+  // const mainCamera = new ArcRotateCamera('maincamera', -Math.PI/2, Math.PI/2, 2, new Vector3(0,0,0), scene);
+  // mainCamera.attachControl();
+
+  const mainCamera = new FreeCamera(
+    'camera',
+    new Vector3(0,0,-2),
+    scene,
+    true
+  );
+  mainCamera.setTarget(Vector3.Zero());
+  mainCamera.attachControl();
+
+  var light = new HemisphericLight(
+    "light1",
+    new Vector3(0, 1, 0),
+    scene
+  );
+  light.intensity = 1.0;
+
+  const manager = new GUI3DManager(scene);
+  const button = new HolographicButton('button');
+  button.text = 'click';
+  button.scaling = new Vector3(0.1, 0.1, 0.2);
+  manager.addControl(button);
+
+  const xr = await scene.createDefaultXRExperienceAsync({
+    uiOptions: {
+      sessionMode: 'immersive-ar'
+    }
+  })
+
+  // scene.createDefaultEnvironment();
 
   engine.runRenderLoop(() => {
     scene.render();
