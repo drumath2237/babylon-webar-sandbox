83a04e777d3858293083f84c75a2dc04d6b2088d
drumath2237 <2237k.drumath@gmail.com>
2021-05-17 13:32:08 +0900

    :sparkles: vite.configにバグがあったのでfix

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,27 @@
 import { defineConfig } from "vite";
 import * as fs from "fs";
 
-const config = defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync("./key.pem"),
-      cert: fs.readFileSync("./cert.pem"),
-    },
-  },
-});
-
-const configGHPages = defineConfig({
-  base: "/babylon-webar-sandbox",
-});
-
 export default ({ command, mode }) => {
   if (mode === "production") {
     console.log("for gh-pages...");
-    return configGHPages;
+
+    const config = defineConfig({
+      base: "/babylon-webar-sandbox",
+    });
+
+    return config;
   } else {
     console.log("for local...");
+
+    const config = defineConfig({
+      server: {
+        https: {
+          key: fs.readFileSync("./key.pem"),
+          cert: fs.readFileSync("./cert.pem"),
+        },
+      },
+    });
+
     return config;
   }
 };
